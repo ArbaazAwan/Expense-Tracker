b0cc87850febfd3acbe599f469200a929d968c47
Arbaaz Veolia <arbaaz.rehman.ext@veolia.com>
2023-03-20 14:49:25 +0500

    added main functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ExpenseTracker';
+  form!: FormGroup;
+  // expenses!: FormArray;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      expenses: new FormArray([])
+    });
+    this.addExpense();
+  }
+
+  newExpense() {
+    return this.fb.group({
+      description:null,
+      entryDate:null,
+      entryAmount:null,
+      tags:null
+    });
+  }
+
+  addExpense() {
+    this.expenses().push(this.newExpense());
+  }
+
+  removeExpense(index: number) {
+    this.expenses().removeAt(index);
+  }
+
+  expenses(): FormArray {
+    return this.form.get('expenses') as FormArray;
+  }
 }
